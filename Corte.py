7af4cd5c97ac1960490cb93e78a2baa79987aa27
JOSE <josebarrundia21@gmail.com>
2021-08-13 20:32:10 -0600

    cambio texto

--- Corte.py
+++ Corte.py
@@ -2,14 +2,10 @@ import requests
 from datetime import datetime
 
 txt = open("RestultadoCorteDiario.txt", "a")
-
-
 # * * * * * cd /home/jose/workspace/python && python Corte.py
 url = "http://192.168.1.26:3000/api/repartidores/emitirCorte2"
 response = requests.get(url)
 
-print(response.text)
-print(response.status_code, response.reason)
 today = datetime.now()
 if response.status_code == 200 :
     txt.writelines("Corte exitoso, msg: "+ response.text +", dia corte: "+ today +" \n" )
